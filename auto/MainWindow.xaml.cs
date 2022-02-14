d199122ed55cb2cb84640f7d0777c84bb185676e
zelenkova.ma.2019 <zelenkova.ma.2019@UC313PC04>
2022-02-14 14:44:12 +0100

    určování tříd

--- auto/MainWindow.xaml.cs
+++ auto/MainWindow.xaml.cs
@@ -25,4 +25,40 @@ namespace auto
             InitializeComponent();
         }
     }
+    class Auto
+    {
+        Random ran = new Random();
+        List<int> Trasa = new List<int>();
+        double _rychlost;
+        int _poloha;
+        enum TypTrasy { normal, most, tunel };
+        double Rychlost { get { return _rychlost; } set { _rychlost = value; } }
+        int Poloha { get { return _poloha; } set { _poloha = value; } }
+        bool Svetla;
+        bool Aktivni;
+        bool Porucha;
+        public void GenTrasy()
+        {
+            Trasa.Clear();
+            int i = ran.Next(1, 51);
+            for (int j = 0; j < i; j++)
+            {
+                Trasa.Add(ran.Next(3));
+            }
+        } 
+    }
+    class MetStanice
+    {
+        double _srazky;
+        double _vitr;
+        double _viditelnost;
+        double Srazky { get { return _srazky; } set { _srazky = value; } }
+        double Vitr { get { return _vitr; } set { _vitr = value; } }
+        Double Viditelnost { get { return _viditelnost; } set { _viditelnost = value; } }
+
+    }
+    class RidiciStanice
+    {
+        
+    }
 }
